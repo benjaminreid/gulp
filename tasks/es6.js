2bb74e9df9422448e3dc8547d5b0045d34d98242
Benjamin Reid <benjamin@nouveller.com>
2015-10-21 10:50:56 +0100

    Basic ES6 task

--- tasks/es6.js
+++ tasks/es6.js
@@ -1,3 +1,27 @@
-module.exports = function() {
-  console.log('compiling from module!');
+var gulp = require('gulp');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
+var streamify = require('gulp-streamify');
+var bower = require('main-bower-files');
+var streamqueue = require('streamqueue');
+
+function scripts(paths) {
+  return browserify({ entries: paths.src, debug: true })
+    .transform(babelify)
+    .bundle()
+    .pipe(source(paths.file));
+}
+
+function dependencies() {
+  var files = bower();
+  return gulp.src(files)
+    .pipe(filter(['*.js']));
+}
+
+module.exports = function(paths) {
+  return streamqueue({ objectMode: true }, dependencies(paths), scripts(paths))
+    .pipe(streamify(concat(paths.file)));
 };
